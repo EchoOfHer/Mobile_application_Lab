c3e55de0d70fcccf09dcc8c811512c6a59ab30f6
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-17 17:32:12 +0700

    Ass@3 : done

--- lib/Lab_week5/Assignment_3.dart
+++ lib/Lab_week5/Assignment_3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Assignment3 extends StatefulWidget {
   const Assignment3({super.key});
@@ -8,6 +9,50 @@ class Assignment3 extends StatefulWidget {
 }
 
 class _Assignment3State extends State<Assignment3> {
+  TextEditingController tcNum = TextEditingController();
+  String buttonText = 'Guess';
+  int answer = Random().nextInt(9) + 1;
+  String result = '';
+  int chance = 3;
+
+  void checking() {
+    if (buttonText == 'Restart') {
+      setState(() {
+        answer = Random().nextInt(9) + 1;
+        chance = 3;
+        result = '';
+        tcNum.clear();
+        buttonText = 'Guess';
+      });
+      return;
+    }
+
+    int? guess = int.tryParse(tcNum.text);
+
+    if (guess == null) return;
+
+    if (guess == answer) {
+      setState(() {
+        result = 'Correct, you win! ';
+        buttonText = 'Restart';
+      });
+    } else {
+      setState(() {
+        chance -= 1;
+        if (guess > answer) {
+          result = '$guess is too large. $chance Chance(s) left!';
+        } else {
+          result = '$guess is too small.  $chance Chance(s) left!';
+        }
+
+        if (chance == 0) {
+          result = 'Sorry you lose. The answer is $answer';
+          buttonText = 'Restart';
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,18 +64,29 @@ class _Assignment3State extends State<Assignment3> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Guess a number game', style: TextStyle(fontSize: 25)),
-                  SizedBox(height: 20),
+                  const Text(
+                    'Guess a number game',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  const SizedBox(height: 20),
                   TextField(
-                    decoration: InputDecoration(
-                      hint: Text('Guess a number 0-9'),
+                    controller: tcNum,
+                    decoration: const InputDecoration(
+                      hintText: 'Guess a number 1-9',
                     ),
+                    keyboardType: TextInputType.number,
                   ),
                 ],
               ),
-              SizedBox(height: 20),
-              Text('result', style: TextStyle(color: Colors.red)),
-              ElevatedButton(onPressed: null, child: Text('data')),
+              const SizedBox(height: 20),
+              Text(result, style: const TextStyle(color: Colors.red)),
+              ElevatedButton(
+                onPressed: checking,
+                child: Text(buttonText),
+                style: ElevatedButton.styleFrom(
+                  side: const BorderSide(color: Colors.deepPurple, width: 1),
+                ),
+              ),
             ],
           ),
         ),
