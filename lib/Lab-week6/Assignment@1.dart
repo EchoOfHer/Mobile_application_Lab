8f63e791b5c54a2fd0f47739b0ba29aa36570ba8
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-24 01:29:22 +0700

    Ass@1 : done

--- lib/Lab-week6/Assignment@1.dart
+++ lib/Lab-week6/Assignment@1.dart
@@ -50,10 +50,88 @@ class Assignment1 extends StatelessWidget {
                       ),
                     ],
                   ),
+                  Spacer(),
                   Row(
                     children: [
                       Icon(Icons.alternate_email, color: primaryColor),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hint: Text(
+                              'Email Address',
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Icon(Icons.lock_open_sharp, color: primaryColor),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hint: Text(
+                              'Password',
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Row(
+                      children: [
+                        Ink(
+                          decoration: const ShapeDecoration(
+                            shape: CircleBorder(
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.android, color: Colors.grey[600]),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Ink(
+                          decoration: const ShapeDecoration(
+                            shape: CircleBorder(
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.message, color: Colors.grey[600]),
+                          ),
+                        ),
+                        Spacer(),
+                        Ink(
+                          decoration: const ShapeDecoration(
+                            color: primaryColor,
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.arrow_forward),
+                            color: bgColor,
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
