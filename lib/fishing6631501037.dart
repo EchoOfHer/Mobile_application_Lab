f584381781fce324068f3c9410484d1164bcf48c
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-25 15:06:31 +0700

    LadTest@1 : done

--- lib/fishing6631501037.dart
+++ lib/fishing6631501037.dart
@@ -1,4 +1,6 @@
 //ID: 6631501037
+import 'dart:ffi';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Fishing extends StatefulWidget {
@@ -10,6 +12,9 @@ class Fishing extends StatefulWidget {
 
 class _FishingState extends State<Fishing> {
   String init_images = 'assets/images/fish/fisherman.png';
+  int quantity = 0;
+  String status = '';
+  int totalPrice = 0;
   int worms = 5;
   // Fish data
   List fish = [
@@ -30,9 +35,82 @@ class _FishingState extends State<Fishing> {
     },
     {'name': 'Shark', 'price': 10, 'image': 'assets/images/fish/shark.png'},
   ];
- Widget createWorms(){
-  return 
- }
+  void fishing() {
+    // // generate a random number min to max
+    // int num = min + Random().nextInt(max - min + 1);
+
+    if (worms != 0) {
+      int randomfish = Random().nextInt(4);
+      int randomNum = 1 + Random().nextInt(6 - 1 + 1);
+      String fishtype = fish[randomfish]['name'];
+      int price = fish[randomfish]['price'];
+      int newPrice = price * randomNum;
+      setState(() {
+        init_images = fish[randomfish]['image'];
+        quantity = randomNum;
+        status = '${fishtype} x ${quantity} = ${newPrice}';
+        totalPrice += newPrice;
+        worms -= 1;
+      });
+    }
+  }
+
+  void reset() {
+    setState(() {
+      totalPrice = 0;
+      status = '';
+      init_images = 'assets/images/fish/fisherman.png';
+      worms = 5;
+    });
+  }
+
+  Widget createWorms(int workTime) {
+    if (workTime == 5) {
+      return OverflowBar(
+        children: [
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+        ],
+      );
+    } else if (workTime == 4) {
+      return OverflowBar(
+        children: [
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+        ],
+      );
+    } else if (workTime == 3) {
+      return OverflowBar(
+        children: [
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+        ],
+      );
+    } else if (workTime == 2) {
+      return OverflowBar(
+        children: [
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+          Icon(Icons.waves, color: Colors.deepOrange, size: 25),
+        ],
+      );
+    } else if (workTime == 1) {
+      return OverflowBar(
+        children: [Icon(Icons.waves, color: Colors.deepOrange, size: 25)],
+      );
+    } else {
+      return OverflowBar(children: [
+          
+        ],
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +127,7 @@ class _FishingState extends State<Fishing> {
             Row(
               children: [
                 Text('Worms: ', style: TextStyle(fontSize: 20)),
-                createWorms(),
+                createWorms(worms),
               ],
             ),
             SizedBox(height: 20),
@@ -57,22 +135,25 @@ class _FishingState extends State<Fishing> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.circle, color: Colors.amber),
-                Text('0', style: TextStyle(fontSize: 20)),
+                Icon(Icons.currency_exchange, color: Colors.amber),
+                SizedBox(width: 10),
+                Text(totalPrice.toString(), style: TextStyle(fontSize: 20)),
               ],
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 60),
               child: Image.asset(init_images),
             ),
+            Text(status, style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: worms > 0 ? fishing : null,
               style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
               child: Text('Fishing', style: TextStyle(color: Colors.white)),
             ),
             SizedBox(height: 8),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: reset,
               style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
               child: Text('Reset', style: TextStyle(color: Colors.white)),
             ),
