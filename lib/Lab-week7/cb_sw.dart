405421acab816de20258a18a90df00f495f4785f
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-10-09 14:05:08 +0700

    Create file

---
+++ lib/Lab-week7/cb_sw.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CbSw extends StatefulWidget {
+  const CbSw({super.key});
+
+  @override
+  State<CbSw> createState() => _CbSwState();
+}
+
+class _CbSwState extends State<CbSw> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:lab_4aug/Lab-week4/basic.dart';
-import 'package:lab_4aug/Lab-week4/input_demo.dart'; //'./Lab-week4/basic.dart';
+import 'package:lab_4aug/Lab-week7/cb_sw.dart'; //'./Lab-week4/basic.dart';
 
 void main() {
-  runApp(MaterialApp(home: InputDemo(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: CbSw(), debugShowCheckedModeBanner: false));
 }
