55bda1bf3c472ccbfb318d3f6478a193f3ea303c
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-10-16 14:56:54 +0700

    Add : Scroll_view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 //import 'package:lab_4aug/Lab-week4/basic.dart';
-import 'package:lab_4aug/Lab-week4/input_demo.dart'; //'./Lab-week4/basic.dart';
+import 'package:lab_4aug/Lab-week8/scrollview_demo.dart'; //'./Lab-week4/basic.dart';
 
 void main() {
-  runApp(MaterialApp(home: InputDemo(), debugShowCheckedModeBanner: false));
+  runApp(
+    MaterialApp(home: ScrollviewDemo(), debugShowCheckedModeBanner: false),
+  );
 }
